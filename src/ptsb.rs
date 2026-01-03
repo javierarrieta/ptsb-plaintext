ed7e35027d5081cb8657884d37c107a9886bda2e
Javier Arrieta <javier@techdelivery.es>
2026-01-03 20:52:42 +0100

    Update libraries to latest

--- src/ptsb.rs
+++ src/ptsb.rs
@@ -1,53 +1,46 @@
 use crate::data::{AppError, StatementLine, TransactionEnum};
-use calamine::DataType::{DateTime, Float, String as DString};
-use calamine::{open_workbook, DataType, Reader, Xls, XlsError};
+use calamine::{Data, open_workbook, Reader, Xls, XlsError};
 use chrono::NaiveDate;
 
 const MS_DAY: i64 = 1000 * 60 * 60 * 24;
 
-impl From<calamine::Error> for AppError {
-    fn from(value: calamine::Error) -> Self {
-        return AppError::InvalidData(value.to_string());
-    }
-}
-
-impl From<calamine::XlsError> for AppError {
+impl From<XlsError> for AppError {
     fn from(value: XlsError) -> Self {
-        return AppError::InvalidData(value.to_string());
+        AppError::InvalidData(value.to_string())
     }
 }
 
-fn parse_date(d: &DataType) -> Option<NaiveDate> {
+fn parse_date(d: &Data) -> Option<NaiveDate> {
     match d {
-        DateTime(date) => {
-            let i: i64 = *date as i64;
+        Data::DateTime(date) => {
+            let i: i64 = date.as_f64() as i64;
             let epoch_days: i32 = (i / MS_DAY) as i32;
-            return NaiveDate::from_epoch_days(epoch_days);
+            NaiveDate::from_epoch_days(epoch_days)
         }
-        DString(date) => {
-            return NaiveDate::parse_from_str(date, "%d/%m/%Y").ok();
+        Data::String(date) => {
+            NaiveDate::parse_from_str(date, "%d/%m/%Y").ok()
         }
-        _ => return None,
+        _ => None,
     }
 }
 
-fn parse_row(d: &[DataType]) -> Option<StatementLine> {
+fn parse_row(d: &[Data]) -> Option<StatementLine> {
     let maybe_date = d.get(0).and_then(parse_date);
     let maybe_description = match d.get(2) {
-        Some(DString(s)) => Some(s),
+        Some(Data::String(s)) => Some(s),
         _ => None,
     };
     let maybe_amount = match (d.get(3), d.get(4)) {
-        (Some(Float(i)), _) => Some(i.to_owned()),
-        (_, Some(Float(out))) => Some(out.to_owned()),
+        (Some(Data::Float(i)), _) => Some(i.to_owned()),
+        (_, Some(Data::Float(out))) => Some(out.to_owned()),
         _ => None,
     };
     let maybe_balance = match d.get(6) {
-        Some(Float(balance)) => Some(balance.to_owned()),
+        Some(Data::Float(balance)) => Some(balance.to_owned()),
         _ => None,
     };
 
-    return match (maybe_date, maybe_description, maybe_amount, maybe_balance) {
+    match (maybe_date, maybe_description, maybe_amount, maybe_balance) {
         (Some(date), Some(desc), Some(amount), Some(balance)) => Some(StatementLine::new(
             date,
             desc.to_string(),
@@ -56,7 +49,7 @@ fn parse_row(d: &[DataType]) -> Option<StatementLine> {
             balance,
         )),
         _ => None,
-    };
+    }
 }
 
 fn guess_tx_type(description: &str) -> Option<TransactionEnum> {
@@ -79,5 +72,5 @@ pub fn parse_file(file_path: &str) -> Result<Vec<StatementLine>, AppError> {
         .map(|r| r.1.to_owned())
         .ok_or(AppError::InvalidData("No sheets present".to_string()))?;
 
-    return Ok(range.rows().filter_map(parse_row).collect());
+    Ok(range.rows().filter_map(parse_row).collect())
 }
